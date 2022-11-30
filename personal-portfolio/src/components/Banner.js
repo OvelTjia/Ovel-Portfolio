4e2b82163b9b74b626b297c2ce068bc9dba42f85
unknown <s2200046@student.unklab.ac.id>
2022-11-30 23:52:58 +0800

    Fix bug dan perapihan

--- personal-portfolio/src/components/Banner.js
+++ personal-portfolio/src/components/Banner.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-// import headerImg from "../assets/img/header-img.svg";
 import headerImg from "../assets/img/profil.png";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import "animate.css";
